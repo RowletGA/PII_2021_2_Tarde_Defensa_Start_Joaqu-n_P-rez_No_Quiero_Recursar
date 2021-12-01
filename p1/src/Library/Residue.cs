84c5d4864d819e910f5340942434998e493c4e2b
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-12-01 19:40:58 -0300

    Ejercicio1 - Corren Test

--- p1/src/Library/Residue.cs
+++ p1/src/Library/Residue.cs
@@ -5,6 +5,16 @@ namespace Ucu.Poo.Defense
         public string Name { get; set; }
 
         public ResidueType Type { get; set; }
+        /// <summary>
+        /// instancio booleano que concuerda con los Test y se da el Get el retorno del mismo
+        /// </summary>
+        /// <value></value>
+        public bool IsOrganic 
+        { 
+            get{
+                return this.Type.IsOrganic; 
+            }
+        }
 
         public Residue(string name, ResidueType type)
         {
