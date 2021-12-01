401f0ac85b68cef2552792b51cddb4320041fda7
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-12-01 19:53:44 -0300

    Ej2 listo

--- p2/src/Library/Offer.cs
+++ p2/src/Library/Offer.cs
@@ -5,8 +5,8 @@ using System.Collections.ObjectModel;
 
 namespace Ucu.Poo.Defense
 {
-    public class Offer
-    {
+    public class Offer 
+    { 
         public DateTime EndDate { get; set; }
 
         public IReadOnlyCollection<OfferItem> Items
@@ -32,6 +32,23 @@ namespace Ucu.Poo.Defense
         public void RemoveItem(OfferItem item)
         {
             this.items.Remove(item);
+        }
+
+        /// <summary>
+        /// intenté darle el mismo formato aunque no era necesario.
+        /// </summary>
+        /// <returns></returns>
+        public string AsText()
+        {
+            StringBuilder ticket = new StringBuilder();
+            ticket.AppendLine($"Fecha : {this.EndDate}\n");
+            foreach (OfferItem item in Items)
+            {
+                ticket.AppendLine($"{item.Quantity} de");
+                ticket.Append(item.Residue.Name);
+                ticket.AppendLine($"a {item.Price} de");
+            }
+            return ticket.ToString();
         }
     }
 }
