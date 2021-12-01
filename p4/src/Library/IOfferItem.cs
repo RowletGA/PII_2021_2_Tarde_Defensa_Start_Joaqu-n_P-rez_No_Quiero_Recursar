1a05496b8f1749ecd0d54914a581331bf35e4fcf
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-12-01 20:32:14 -0300

    se intentó :(

---
+++ p4/src/Library/IOfferItem.cs
@@ -0,0 +1,10 @@
+namespace Ucu.Poo.Defense
+{
+    /// <summary>
+    /// buscar que tienen en común estas clases y tirarlas a la interfaz
+    /// </summary>
+    public interface IOfferItem
+    {
+       public int SubTotal { get; }
+    }
+}
--- p4/src/Library/Offer.cs
+++ p4/src/Library/Offer.cs
@@ -9,11 +9,11 @@ namespace Ucu.Poo.Defense
     {
         public DateTime EndDate { get; set; }
 
-        public IReadOnlyCollection<OfferItem> Items
+        public IReadOnlyCollection<IOfferItem> Items
         {
             get
             {
-                return new ReadOnlyCollection<OfferItem>(this.items);
+                return new ReadOnlyCollection<IOfferItem>(this.items);
             }
         }
 
@@ -22,7 +22,7 @@ namespace Ucu.Poo.Defense
             get
             {
                 double result = 0;
-                foreach (OfferItem item in this.items)
+                foreach (IOfferItem item in this.items)
                 {
                     result = result + item.SubTotal;
                 }
@@ -38,7 +38,7 @@ namespace Ucu.Poo.Defense
             this.EndDate = endDate;
         }
 
-        public OfferItem AddItem(Residue Residue, int quantity, int price)
+        public  IOfferItem AddItem(Residue Residue, int quantity, int price)
         {
             OfferItem item = new OfferItem(Residue, quantity, price);
             this.items.Add(item);
@@ -48,6 +48,12 @@ namespace Ucu.Poo.Defense
         public void RemoveItem(OfferItem item)
         {
             this.items.Remove(item);
+        }
+        public IOfferItem AddDiscount(double discount)
+        {
+            IOfferItem item = new OfferItem(discount);
+            this.items.Add(item);
+            return item;
         }
     }
 }
